f781cdaeb2a66f55178629b56b3f05c2ea8b7af4
Nemchinov Sergey <googoodemon@gmail.com>
2020-05-04 17:51:30 +0700

    реализация тестовой задачи

---
+++ src/main/java/com/nsergey/trainings/algorithms/task0/StringLength.java
@@ -0,0 +1,24 @@
+package com.nsergey.trainings.algorithms.task0;
+
+import com.nsergey.trainings.algorithms.Task;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StringLength implements Task {
+
+    @Override
+    public String run(List<String> data) {
+        return String.valueOf(data.get(0).length());
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Usage: <data>");
+            return;
+        }
+        String result = new StringLength().run(Arrays.asList(args));
+        System.out.println("Result: " + result);
+    }
+
+}
