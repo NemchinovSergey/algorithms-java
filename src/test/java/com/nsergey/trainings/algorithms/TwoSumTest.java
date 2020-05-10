01a3ba9603f86066574ea7fbdda19528f750135f
Nemchinov Sergey <googoodemon@gmail.com>
2020-05-10 20:55:42 +0700

    добавил недостающие тесты

--- src/test/java/com/nsergey/trainings/algorithms/TwoSumTest.java
+++ src/test/java/com/nsergey/trainings/algorithms/TwoSumTest.java
@@ -1,6 +1,8 @@
 package com.nsergey.trainings.algorithms;
 
-import com.nsergey.trainings.algorithms.taskTwoSum.TwoSum;
+import com.nsergey.trainings.algorithms.twosum.TwoSumBruteForce;
+import com.nsergey.trainings.algorithms.twosum.TwoSumOnePassHashTable;
+import com.nsergey.trainings.algorithms.twosum.TwoSumTwoPassHashTable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,10 +10,22 @@ import static com.nsergey.trainings.algorithms.TestUtils.runTest;
 
 public class TwoSumTest {
 
-    @DisplayName("LeetCode - Two Sum")
+    @DisplayName("LeetCode - Two Sum with brute force")
     @Test
-    public void testTwoSum() {
-        runTest(new TwoSum(), "TwoSum");
+    public void testTwoSumBruteForce() {
+        runTest(new TwoSumBruteForce(), "TwoSum");
+    }
+
+    @DisplayName("LeetCode - Two Sum with two pass hash table")
+    @Test
+    public void testTwoSumTwoPassHashTable() {
+        runTest(new TwoSumTwoPassHashTable(), "TwoSum");
+    }
+
+    @DisplayName("LeetCode - Two Sum with one pass hash table")
+    @Test
+    public void testTwoSumOnePassHashTable() {
+        runTest(new TwoSumOnePassHashTable(), "TwoSum");
     }
 
 }
